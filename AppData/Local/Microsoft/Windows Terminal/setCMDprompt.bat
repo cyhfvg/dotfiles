b3840813310d90f7f5bb3786f5d3239774b2a1f7
cyhfvg <code.guangzhou.zhang@outlook.com>
2023-03-09 13:54:07 +0800

    feat(`windows terminal`): split panel with CWD

---
+++ AppData/Local/Microsoft/Windows Terminal/setCMDprompt.bat
@@ -0,0 +1,7 @@
+REM set prompt
+set PROMPT=$e]9;9;$P$e\%PROMPT%
+
+REM persistent
+setx PROMPT "%PROMPT%"
+
+REM pause
---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\tokyonight_storm.omp.json" --print) -join "`n"))
+
+# scoop search hook
+Invoke-Expression (&scoop-search --hook)
+
+# escape windows terminal starting directory prompt
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  return $out
+}
